29dc4197570e737704bc0c1f87b693b95035710a
Junaid Qadir Shekhanzai <junaidqadirb@gmail.com>
2025-07-26 20:27:45 +0500

    Allow toggling events display for privacy

--- Countdown/CountdownEvent.swift
+++ Countdown/CountdownEvent.swift
@@ -6,11 +6,13 @@ struct CountdownEvent: Identifiable, Codable, Equatable {
 var emoji: String
     var label: String
     var date: Date
+    var isPrivate: Bool
 
-    init(id: UUID = UUID(), emoji: String, label: String, date: Date) {
+    init(id: UUID = UUID(), emoji: String, label: String, date: Date, isPrivate: Bool = false) {
         self.id = id
         self.emoji = emoji
         self.label = label
         self.date = date
+        self.isPrivate = isPrivate
     }
 }
